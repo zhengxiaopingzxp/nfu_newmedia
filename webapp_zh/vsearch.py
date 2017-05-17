9ab1a4427b0754a14c0e19224c9df71bd26bae8c
Zhou-Yu-Jin <1119235833@qq.com>
2017-05-17 20:15:06 +0800

    2017.5.17

--- webapp_zh/vsearch.py
+++ webapp_zh/vsearch.py
@@ -9,3 +9,4 @@ def search4prepositions_zhs(phrase: str) -> set:
 def search4letters(phrase: str, letters: str='临乎与为共冲到兜于即从以似假去让诸及往迆连迎道遵对导寻将当叫吃合同向和问如尽打执把投拦按捉洎给维缘在因惟就比照较方爿暨拿替望朝爰直由率被用繇齐至管自起趁践跟') -> set:
     """Return a set of the 'letters' found in 'phrase'."""
     return set(letters).intersection(set(phrase))
+print(vsearch.py)
